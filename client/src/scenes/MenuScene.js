d4adc425e8b0798ba2f8a221114c65311ad4e4a3
gezeebeezee <57654189+gezeebeezee@users.noreply.github.com>
2024-10-23 14:05:19 -0700

    Added sprite icon to hover over mode selection. Mode selection enabled on mouse-click.

--- client/src/scenes/MenuScene.js
+++ client/src/scenes/MenuScene.js
@@ -8,6 +8,7 @@ export class MenuScene extends Phaser.Scene {
 
     preload() {
         this.load.image('space', "../assets/backgrounds/Space01.png")
+        this.load.image('ship', "../assets/sprites/Spaceship01.png")
     }
 
     create() {
@@ -45,11 +46,51 @@ export class MenuScene extends Phaser.Scene {
                 pvc.setFontFamily('"Press Start 2P"').setColor('#ffffff');
             }
         })
+
+        // Add sprite to indicate menu selection
+        let hoverSprite = this.add.image(100,100,'ship')
+        hoverSprite.setScale(1);
+        hoverSprite.setVisible(false);
+
+
+        // Add interactivity on hover for menu selection
+        // Player vs Player
+        pvp.setInteractive();
+        pvp.on('pointerover', () => {
+            console.log('pvp hover')
+            hoverSprite.setVisible(true);
+            hoverSprite.x = pvp.x - pvp.width/2 - 40;
+            hoverSprite.y = pvp.y;
+        })
+        pvp.on('pointerout', () => {
+            console.log('pvp out')
+            hoverSprite.setVisible(false);
+        })
+
+        pvp.on('pointerdown', () => {
+            this.scene.start('playGame')
+        })
+
+        // Player vs Computer
+        pvc.setInteractive();
+
+        pvc.on('pointerover', () => {
+            hoverSprite.setVisible(true);
+            hoverSprite.x = pvc.x - pvc.width/2 - 40;
+            hoverSprite.y = pvc.y;
+            console.log('pvc hover')
+        })
+
+        pvc.on('pointerout', () => {
+            hoverSprite.setVisible(false);
+            console.log('pvc out')
+        })
+
+        pvc.on('pointerdown', () => {
+            this.scene.start('playGame')
+        })
     }
 
-    update() {
-        this.input.keyboard.on('keydown-ENTER', function() {
-            this.scene.start('playGame')
-        }, this)
+    update() {       
     }
 }
