7ccb5317de7aaf81b409571d2f9ba005249cd0f1
Dawn Arrington <dawn.arrington@gmail.com>
2024-10-28 21:07:37 -0400

    added asteroids calibration handles and functional explosions and degredation

--- client/src/scenes/GameScene.js
+++ client/src/scenes/GameScene.js
@@ -9,11 +9,22 @@ export class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('laser', '../assets/sprites/laser_rotated.png');
         this.load.image('meteor', '../assets/sprites/Asteroids.png');
-        this.load.spritesheet('spaceship', '../assets/sprites/Spaceship2_rotated.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('spaceship', '../assets/sprites/Spaceship2_rotated.png', { frameWidth: 24, frameHeight: 24 });
+        this.load.spritesheet('asteroid', '../assets/sprites/Asteroid 01 - Explode.png', { frameWidth: 90, frameHeight: 90, frame: 0 });
 
     }
 
     create() {
+        // Calibrations
+        const asteroids_x_vel_min = -5;
+        const asteroids_x_vel_max = 5;
+        const asteroids_y_vel_min = -5;
+        const asteroids_y_vel_max = 5;
+        const asteroids_x_coverage = 200;
+        const num_asteroids = 50;
+        const asteroids_frame_rate = 30;
+
+        // Init
         this.gameOver = false;
 
         //  A simple background for our game
@@ -25,12 +36,28 @@ export class GameScene extends Phaser.Scene {
         image.setScale(scale).setScrollFactor(0)
 
         
-        // // The player and its settings
+        // The player and its settings
         this.player = this.physics.add.sprite(100, 450, 'spaceship');
         this.player.setCollideWorldBounds(true);
 
-        // //  Animations
+        //  Animations
+        this.anims.create(
+            {
+                key: "degredation",
+                frames: this.anims.generateFrameNumbers('asteroid', { start: 0, end: 1 }),
+                frameRate: asteroids_frame_rate
+            }
+        );
 
+        this.anims.create(
+            {
+                key: "explosion",
+                frames: this.anims.generateFrameNumbers('asteroid', { start: 2, end: 6 }),
+                frameRate: asteroids_frame_rate,
+                repeat: 0,
+                hideOnComplete: true
+            }
+        );
 
         // // Lasers
         // lasers = this.physics.add.group({
@@ -39,30 +66,32 @@ export class GameScene extends Phaser.Scene {
         // });
 
 
-        // // Generate meteors
-        let meteors = this.physics.add.group({
-            key: 'meteor',
-            repeat: 50
+        // Generate meteors
+        this.meteors = this.physics.add.group({
+            key: 'asteroid',
+            repeat: num_asteroids
         });
 
-        meteors.children.iterate(function (child,player) {
-            var rand_x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-            var rand_y = (player.y < 300) ? Phaser.Math.Between(300, 600) : Phaser.Math.Between(0, 300);
+        this.meteors.children.iterate(function (child) {
+            var rand_x = Phaser.Math.Between(400 - asteroids_x_coverage, 400 + asteroids_x_coverage);
+            var rand_y = Phaser.Math.Between(0, 600);
+
             child.setPosition(rand_x,rand_y);
-            child.setVelocity(Phaser.Math.Between(-10, 10), Phaser.Math.Between(-8, 8));
+            child.setVelocity(Phaser.Math.Between(asteroids_x_vel_min, asteroids_x_vel_max), Phaser.Math.Between(asteroids_y_vel_min, asteroids_y_vel_max));
             child.allowGravity = false;
-
         });
-        // //  Input Events
+
+        //  Input Events
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        // //  The score
+        //  The score
         this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#4488aa' });
 
 
-        // // Hit by meteor. Game over
+        // Hit by meteor. Game over
         this.physics.add.collider(this.player, this.meteors, this.hitByMeteor, null, this);
 
+        // Hit meteor with laser
         // this.physics.add.collider(laser, enemy, shoot)
         
         }
@@ -101,8 +130,9 @@ export class GameScene extends Phaser.Scene {
         {
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
-        }        
+        }  
     }
+
 
     // shootLaser() {
     //     lasers.setVelocityX(100)
@@ -119,11 +149,25 @@ export class GameScene extends Phaser.Scene {
     //     // score +=50
     // }
 
-    hitByMeteor() {
+    hitByMeteor(player,meteor) {
+        
+        // Ship and meteor explodes
+        meteor.play("explosion")
+        player.play("explosion")
         this.physics.pause();
-
         this.player.setTint(0xff0000);
-
         this.gameOver = true;
+        
+    }
+
+    destroyMeteor(player,meteor) {
+        console.log(meteor.anims.currentFrame)
+        if (meteor.anims.currentFrame != null) {
+            meteor.play("explosion")
+        }
+        else {
+            meteor.play("degredation")
+            
+        }
     }
 }
