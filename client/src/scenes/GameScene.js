56465e0198acbece42c9e2a787af095589a78cbd
gezeebeezee <57654189+gezeebeezee@users.noreply.github.com>
2024-10-30 18:35:12 -0700

    Added vertical spaceship movement and laser firing. Laser firing in progress.

--- client/src/scenes/GameScene.js
+++ client/src/scenes/GameScene.js
@@ -1,5 +1,15 @@
 import Phaser from '../../lib/phaser.js'
 
+// Player Configuration
+const shipSpeed = 150;
+
+// Laser Configuration
+const laserMax = 10;
+const laserInterval = 300;
+const laserSpeed = 300;
+const laserLifespan = 3;
+var laserDelay = laserInterval;
+
 export class GameScene extends Phaser.Scene {
     constructor() {
         super('playGame');
@@ -10,6 +20,7 @@ export class GameScene extends Phaser.Scene {
         this.load.image('laser', '../assets/sprites/laser_rotated.png');
         this.load.image('meteor', '../assets/sprites/Asteroids.png');
         this.load.spritesheet('spaceship', '../assets/sprites/Spaceship2_rotated.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('spaceship2', '../assets/sprites/Spaceship01-rotated.png', {frameWidth: 32, frameHeight: 48});
 
     }
 
@@ -25,24 +36,33 @@ export class GameScene extends Phaser.Scene {
         image.setScale(scale).setScrollFactor(0)
 
         
-        // // The player and its settings
-        this.player = this.physics.add.sprite(100, 450, 'spaceship');
+        // // Add player 1 (Left)
+        this.player = this.physics.add.sprite(25, 300, 'spaceship');
         this.player.setCollideWorldBounds(true);
+
+        // Add Player 2 (Right)
+        this.player2 = this.physics.add.sprite(this.scale.width - 25, 300, 'spaceship2');
+        this.player2.setCollideWorldBounds(true);
 
         // //  Animations
 
 
         // // Lasers
-        // lasers = this.physics.add.group({
-        //     key: 'laser',
-        //     setXY: {x: player.x, y: player.y},
-        // });
-
+        this.laserGroup = this.physics.add.group({
+            name: 'lasers-${Phaser.Math.RND.uuid()}',
+            enable: false,
+        });
+        this.laserGroup.createMultiple({
+            key: 'laser',
+            quantity: laserMax,
+            active: false,
+            visible: false,
+        });
 
         // // Generate meteors
         let meteors = this.physics.add.group({
             key: 'meteor',
-            repeat: 50
+            repeat: 10
         });
 
         meteors.children.iterate(function (child,player) {
@@ -67,36 +87,31 @@ export class GameScene extends Phaser.Scene {
         
         }
 
-    update() {
+    update(ts, dt) {
         if (this.gameOver)
             {
                 return;
             }
+
+        laserDelay -= dt;
         if (this.cursors.space.isDown)
         {
-            // shootLaser();
-    
+            if (laserDelay > 0) {
+                return
+            }
+
+            this.fireLaser()
         }
+
         if (this.cursors.up.isDown)
         {
-            this.player.setVelocityY(-160);
-    
+            this.player.setVelocityY(-shipSpeed)    
         }
         else if (this.cursors.down.isDown)
         {
-            this.player.setVelocityY(160);
+            this.player.setVelocityY(shipSpeed);
     
-        }
-        else if (this.cursors.left.isDown)
-        {
-            this.player.setVelocityX(-160);
-    
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.setVelocityX(160);
-        }
-    
+        }    
         else
         {
             this.player.setVelocityX(0);
@@ -125,5 +140,19 @@ export class GameScene extends Phaser.Scene {
         this.player.setTint(0xff0000);
 
         this.gameOver = true;
+    }
+
+    fireLaser() {
+        const laser = this.laserGroup.getFirstDead();
+        if (laser === undefined || laser === null) {
+            return;
+        }
+
+        const x = this.player.x + 20;
+        const y = this.player.y;
+        laser.enableBody(true, x, y, true, true);
+        laser.body.velocity.x += 300;
+
+        laserDelay = laserInterval;
     }
 }
